549c2d9b7b5a318944ba1c7ae95a1d86339464df
Gustavo Paiva <contatogppe@gmail.com>
2025-03-11 21:46:57 -0300

    backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');                 //express
+const errorHandler = require('./middlewares/errorHandler'); 
+
+try {
+  require('./config/database').authenticate() 
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./config/swagger');
+
+
+const app = express();
+app.use(express.json());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs)); //documentação em tempo real, serve para testes também
+
+app.use(errorHandler);    //tratamento de erros
+
+app.listen(4000, () => {
+  console.log('Servidor rodando na porta 4000');
+});
